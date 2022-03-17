ecf42527e0943ccaf21eb835f5a66953ae97eb3b
overmellow <morteza_faraji@yahoo.com>
2022-03-17 14:39:50 -0700

    first commit

---
+++ Gitpod/src/main/java/com/wv/mfaraji/gitpod/Gitpod/App.java
@@ -0,0 +1,13 @@
+package com.wv.mfaraji.gitpod.Gitpod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
